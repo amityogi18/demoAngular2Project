0f9b7f23aedfd7235f2ed962835d49a696020156
Amit Yogi <gauravgautam@i-techsoftware.com>
2018-02-19 20:23:27 +0530

    Completed the Delete Part

--- src/app/hostname-details/hostname-details.component.ts
+++ src/app/hostname-details/hostname-details.component.ts
@@ -14,16 +14,11 @@ export class HostnameDetailsComponent implements OnInit {
   newModel;
   mode;
   selectedObject;
-   constructor(public utility: UtilityService) {
-  this.showGetHostName();
-  this.model = {
-    hostname: '',
-    loopback: '',
-    };
-    this.newModel = {
-      interface: '',
-      ip: ''
-    };
+  constructor(public utility: UtilityService) {
+    this.showGetHostName();
+    this.username = [];
+    this.resetModel();
+    this.resetNewModel();
     this.selectedObject = {};
     this.mode = 'add';
   }
@@ -31,17 +26,36 @@ export class HostnameDetailsComponent implements OnInit {
   ngOnInit() {
   }
 
+  setLocalStorage(param) {
+    if (param) {
+      localStorage.setItem('listHostname', JSON.stringify(param));
+    }
+  }
+
+  resetModel() {
+    this.model = {
+      hostname: '',
+      loopback: '',
+    };
+  }
+
+  resetNewModel() {
+    this.newModel = {
+      interface: '',
+      ip: ''
+    };
+  }
+
   showGetHostName() {
     this.utility.getHostName().subscribe(
-      data => this.processData(data) ,
+      data => this.processData(data),
       error => this.handleError(error)
     );
   }
 
   processData(data) {
     if (_.isEmpty(localStorage.getItem('listHostname'))) {
-  //  if (localStorage.getItem('listHostname') == '' || localStorage.getItem('listHostname') == undefined) {
-      localStorage.setItem('listHostname', JSON.stringify(data.data));
+      this.setLocalStorage(data.data);
     }
     this.username = JSON.parse(localStorage.getItem('listHostname'));
   }
@@ -53,68 +67,57 @@ export class HostnameDetailsComponent implements OnInit {
   addHostName() {
     this.model.id = this.username.length + 1;
     this.username.push(this.model);
-    this.model =  {
-      hostname: '',
-      loopback: ''
-    };
-      localStorage.setItem('listHostname', JSON.stringify(this.username));
-      console.log(this.username);
+    this.resetModel();
+    this.setLocalStorage(this.username);
   }
 
   editHostName() {
     this.username[this.model.id - 1] = this.model;
-   // console.log('updated data', this.username[this.model.id - 1]);
-    localStorage.setItem('listHostname', JSON.stringify(this.username));
-    this.model =  {
-      hostname: '',
-      loopback: ''
-    };
+    this.setLocalStorage(this.username);
+    this.resetModel();
     this.mode = 'add';
   }
 
   onEditClick(editData) {
     this.mode = 'edit';
-    console.log(editData);
     this.model = editData;
   }
 
 
-  deleteHostName(value) {
-    this.username.splice((value.id - 1), 1);
-    localStorage.setItem('listHostname', JSON.stringify(this.username));
+  deleteHostName(value: number) {
+    this.username.splice(value, 1);
+    this.setLocalStorage(this.username);
   }
 
   viewHostName(viewData) {
-     this.selectedObject = viewData;
+    this.selectedObject = viewData;
   }
 
   addInterface() {
     this.newModel.sno = this.selectedObject.arr.length + 1;
     this.selectedObject.arr.push(this.newModel);
-    this.username[this.selectedObject.id -1] = this.selectedObject;
-    this.newModel = {
-      interface: '',
-      ip: ''
-    };
-    localStorage.setItem('listHostname', JSON.stringify(this.username));
+    this.username[this.selectedObject.id - 1] = this.selectedObject;
+    this.resetNewModel();
+    this.setLocalStorage(this.username);
   }
 
   editInterface() {
     this.selectedObject.arr[this.newModel.sno - 1] = this.newModel;
     this.username[this.selectedObject.id - 1] = this.selectedObject;
-    localStorage.setItem('listHostname', JSON.stringify(this.username));
-    this.newModel = {
-      interface: '',
-      ip: ''
-    };
+    this.setLocalStorage(this.username);
+    this.resetNewModel();
     this.mode = 'add';
-
   }
 
   onEditInterface(editValue) {
     this.mode = 'edit';
     this.newModel = editValue;
+  }
 
+  onDeleteInterface(index: number) {
+    this.selectedObject.arr.splice(index, 1);
+    this.username[this.selectedObject.id - 1] = this.selectedObject;
+    this.setLocalStorage(this.username);
   }
 
 }
